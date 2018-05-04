e2357c388ead64672bafd82045d63f16fc330c55
LauraTCM <ltortosa@edu.tecnocampus.cat>
2018-05-04 09:26:13 +0200

    SLIDE 42

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -36,15 +36,15 @@ public class BowlingGameTest {
 
     }
 
-//    @Test
-//    public void testOneSpare() throws  Exception{
-//        g.roll(5);
-//        g.roll(5);
-//        g.roll(3);
-//        rollMany(17,0);
-//        assertEquals(16, g.score());
-//
-//    }
+    @Test
+    public void testOneSpare() throws  Exception{
+        g.roll(5);
+        g.roll(5);
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(16, g.score());
+
+    }
 
 
 
